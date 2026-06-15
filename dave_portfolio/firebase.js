00494f27d1b14cd149ee89dc01d3be0314a85d4f
codesdave <mr.dave25@yahoo.com>
2026-06-15 13:03:17 +0100

    firebase updated

--- dave_portfolio/firebase.js
+++ dave_portfolio/firebase.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAHslzTIem61NbSGMFotzoyrnt81Nc8nlI",
     authDomain: "asp-dave-portfolio.firebaseapp.com",
