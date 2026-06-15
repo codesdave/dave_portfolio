8ffc15135ed2fb5ce8a7f7b384f900eb635f9db1
codesdave <mr.dave25@yahoo.com>
2026-06-15 12:06:42 +0100

    add Firebase config and Firestore backend

---
+++ dave_portfolio/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHslzTIem61NbSGMFotzoyrnt81Nc8nlI",
+    authDomain: "asp-dave-portfolio.firebaseapp.com",
+    projectId: "asp-dave-portfolio",
+    storageBucket: "asp-dave-portfolio.firebasestorage.app",
+    messagingSenderId: "737873120964",
+    appId: "1:737873120964:web:bdfd842968f8428aeb8391",
+    measurementId: "G-SZDHZLLEDJ"
+  };
